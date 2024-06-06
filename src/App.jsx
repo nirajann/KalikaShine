9f3da229837020ef25ff10724f01820e3fd573a8
Nirajan gautam <78842151+nirajann@users.noreply.github.com>
2024-06-06 13:13:52 +0545

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <Router>
       {/* <Header/> */}
       <Routes>
-        <Route path="/KalikaShine/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/*" element={<Notfound />} />
       </Routes>
 
